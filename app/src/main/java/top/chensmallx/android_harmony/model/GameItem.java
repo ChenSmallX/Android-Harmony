8588663cf4855d2a0bc4a0de2feaff38e4b827d6
Maymomo <3466365807@qq.com>
2020-01-03 10:55:26 +0800

    fix name style

--- app/src/main/java/top/chensmallx/android_harmony/model/GameItem.java
+++ app/src/main/java/top/chensmallx/android_harmony/model/GameItem.java
@@ -2,32 +2,32 @@ package top.chensmallx.android_harmony.model;
 
 public class GameItem {
 
-    private String game_name_chinese;
-    private String game_name_english;
-    private String game_price_curr;
-    private String game_price_ori;
-    private String game_price_coin = "CNY";
+    private String gameNameChinese;
+    private String gameNameEnglish;
+    private String gamePriceCurr;
+    private String gamePriceOri;
+    private String gamePriceCoin = "CNY";
 
-    private String game_cover_url;
+    private String gameCoverUrl;
 
 
     public GameItem() {
         super();
     }
 
-    public GameItem(String game_name_chinese_,
-                    String game_name_english_,
-                    String game_price_curr_,
-                    String game_price_ori_,
-                    String game_price_coin_,
-                    String game_cover_url_) {
+    public GameItem(String gameNameChinese,
+                    String gameNameEnglish,
+                    String gamePriceCurr,
+                    String gamePriceOri,
+                    String gamePriceCoin,
+                    String gameCoverUrl) {
         super();
 
-        game_name_chinese   = game_name_chinese_;
-        game_name_english   = game_name_english_;
-        game_price_curr     = game_price_curr_;
-        game_price_ori      = game_price_ori_;
-        game_price_coin     = game_price_coin_;
-        game_cover_url      = game_cover_url_;
+        this.gameNameChinese = gameNameChinese;
+        this.gameNameEnglish = gameNameEnglish;
+        this.gamePriceCurr = gamePriceCurr;
+        this.gamePriceOri = gamePriceOri;
+        this.gamePriceCoin = gamePriceCoin;
+        this.gameCoverUrl = gameCoverUrl;
     }
 }
